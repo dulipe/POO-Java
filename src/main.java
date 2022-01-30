674d5de25c00599d3f49c2202e7ae905789afba8
dulipe <filipe_opc2@hotmail.com>
2022-01-30 18:44:40 -0300

    Java Dio

---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Cliente filipe = new Cliente();
+		filipe.setNome("Filipe");
+		
+		
+		Conta cc = new ContaCorrente(filipe);
+		Conta poupanca = new ContaPoupanca(filipe);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
